91d847c1679fc9c936a8c03ffcf123358e907a9b
Cliff Seal <cliff@logos-creative.com>
2014-05-06 07:26:41 -0400

    Roll WP Skeleton

--- wp.sh
+++ wp.sh
@@ -2,11 +2,8 @@
 cd /Applications/MAMP/htdocs
 printf "What would you like to name your new WordPress root directory (i.e. mywpdir)? "
 read NEWDIR
-git clone https://github.com/WordPress/WordPress.git $NEWDIR
+git clone git@github.com:logoscreative/WordPress-Skeleton.git $NEWDIR --recursive
 cd $NEWDIR
-printf "What version of WordPress would you like to use? It must be a non-beta release (i.e. 3.4.2). "
-read WPVER
-git checkout $WPVER
 rm -rf .git
 printf "MySQL User: "
 read MYSQLUSER
@@ -23,15 +20,15 @@ if [ -f ./wp-config.php ]
 then
 open http://localhost:8888/$NEWDIR/wp-admin/install.php
 else
-cp -n ./wp-config-sample.php ./wp-config.php
+cp -n ./local-config-sample.php ./local-config.php
 SECRETKEYS=$(curl -L https://api.wordpress.org/secret-key/1.1/salt/)
 EXISTINGKEYS='put your unique phrase here'
 printf '%s\n' "g/$EXISTINGKEYS/d" a "$SECRETKEYS" . w | ed -s wp-config.php
-DBUSER=$"username_here"
-DBPASS=$"password_here"
-DBNAME=$"database_name_here"
-sed -i '' -e "s/${DBUSER}/${MYSQLUSER}/g" wp-config.php
-sed -i '' -e "s/${DBPASS}/${MYSQLPWD}/g" wp-config.php
-sed -i '' -e "s/${DBNAME}/${NEWDB}/g" wp-config.php
+DBUSER=$"local_db_user"
+DBPASS=$"local_db_password"
+DBNAME=$"local_db_name"
+sed -i '' -e "s/${DBUSER}/${MYSQLUSER}/g" local-config.php
+sed -i '' -e "s/${DBPASS}/${MYSQLPWD}/g" local-config.php
+sed -i '' -e "s/${DBNAME}/${NEWDB}/g" local-config.php
 open http://localhost:8888/$NEWDIR/wp-admin/install.php
 fi
